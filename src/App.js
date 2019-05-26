e2b800abc934c8f1d4036d499ac58c04d6bd235d
xinyd <xinyd1992@gmail.com>
2019-05-26 20:14:38 +0800

    docker

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2019-5-26!
         </a>
       </header>
     </div>
